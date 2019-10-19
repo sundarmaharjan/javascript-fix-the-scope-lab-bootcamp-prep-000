5c2c5e3def0d9f771c47e40ab1ee1addd87b1d62
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-19 13:53:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function yourAnimal() {
 console.log('yourAnimal');
   return animal
 }
-
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
